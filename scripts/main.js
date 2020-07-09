c5bde2f637f6afd23743818476918601f3a469d2
Oliver Bucher <level8broccoli@gmail.com>
2020-07-09 21:34:01 +0200

    add Compositor

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ import {
   loadKeys,
   loadSprites
 } from "./loaders.js";
+import Compositor from "./Compositor.js";
 
 function drawBackground(type, x, y, ctx, sprites) {
   sprites.drawTile(type, ctx, x, y);
@@ -17,35 +18,53 @@ const ctx = canvas.getContext('2d');
 canvas.width = GAME_WIDTH * TILE_SIZE;
 canvas.height = GAME_HEIGHT * TILE_SIZE;
 
+function createBackgroundLayer(background, sprites) {
+  const buffer = document.createElement('canvas');
+  buffer.width = GAME_WIDTH * TILE_SIZE;
+  buffer.height = GAME_HEIGHT * TILE_SIZE;
+
+  background.forEach((row, y) => {
+    row.forEach((tile, x) => {
+      drawBackground(tile, x, y, buffer.getContext('2d'), sprites);
+    });
+  });
+
+  return function drawBackgroundLayer(ctx) {
+    ctx.drawImage(buffer, 0, 0);
+  };
+}
+
+function createPlayerLayer(sprites, pos) {
+  return function drawPlayerLayer(ctx) {
+    sprites.drawTile('star', ctx, pos.x, pos.y);
+  };
+}
+
 Promise.all([
   loadSprites(TILE_SIZE),
   loadKeys(),
   loadLevel('1-1')
 ]).then(([sprites, map, level]) => {
+  const comp = new Compositor();
   sprites.defineAllSprites(map);
 
-  const backgroundBuffer = document.createElement('canvas');
-  backgroundBuffer.width = GAME_WIDTH * TILE_SIZE;
-  backgroundBuffer.height = GAME_HEIGHT * TILE_SIZE;
-
-  level.background.forEach((row, y) => {
-    row.forEach((tile, x) => {
-      drawBackground(tile, x, y, backgroundBuffer.getContext('2d'), sprites);
-    });
-  });
+  const backgroundLayer = createBackgroundLayer(level.background, sprites);
+  comp.layers.push(backgroundLayer);
 
   const playerPos = {
     x: 2,
     y: 2,
   };
+  const playerLayer = createPlayerLayer(sprites, playerPos);
+  comp.layers.push(playerLayer);
+
 
   function update() {
-    ctx.drawImage(backgroundBuffer, 0, 0);
-    sprites.drawTile('star', ctx, playerPos.x, playerPos.y);
+    comp.draw(ctx);
+
     playerPos.x += 0.2;
     playerPos.y += 0.2;
     requestAnimationFrame(update);
   }
   update();
-
 });
