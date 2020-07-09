3fc08bb7214822454446e7ee334aa2e79de21c35
Oliver Bucher <level8broccoli@gmail.com>
2020-07-09 21:18:14 +0200

    refactoring

--- scripts/loaders.js
+++ scripts/loaders.js
@@ -1,4 +1,6 @@
-export function loadImage(url) {
+import SpriteSheet from './sprites/SpriteSheet.js';
+
+function loadImage(url) {
   return new Promise(resolve => {
     const image = new Image();
     image.addEventListener('load', () => resolve(image));
@@ -12,4 +14,11 @@ export function loadLevel(name) {
 
 export function loadKeys(url = '/scripts/sprites/sprite-map.json') {
   return fetch(url).then(r => r.json());
+}
+
+export function loadSprites(size, url = '/assets/tileset.png') {
+  return loadImage(url).then(image => {
+    const sprites = new SpriteSheet(image, size, size);
+    return sprites;
+  });
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,16 @@
-import SpriteSheet from './sprites/SpriteSheet.js';
 import {
-  loadImage,
   loadLevel,
-  loadKeys
+  loadKeys,
+  loadSprites
 } from "./loaders.js";
 
 function drawBackground(type, x, y, ctx, sprites) {
   sprites.drawTile(type, ctx, x, y);
 }
 
-function loadSprites() {
-  return loadImage(TILESET_URL).then(image => {
-    const sprites = new SpriteSheet(image, TILE_SIZE, TILE_SIZE);
-    return sprites;
-  });
-}
-
 const TILE_SIZE = 32;
 const GAME_WIDTH = 24;
 const GAME_HEIGHT = 24;
-const TILESET_URL = '/assets/tileset.png';
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -27,7 +18,7 @@ canvas.width = GAME_WIDTH * TILE_SIZE;
 canvas.height = GAME_HEIGHT * TILE_SIZE;
 
 Promise.all([
-  loadSprites(),
+  loadSprites(TILE_SIZE),
   loadKeys(),
   loadLevel('1-1')
 ]).then(([sprites, map, level]) => {
