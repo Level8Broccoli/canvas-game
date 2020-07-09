51db4d0c8ab47c93e5b578c5b3089311aa189d6c
Oliver Bucher <level8broccoli@gmail.com>
2020-07-09 21:26:15 +0200

    update canvas

--- scripts/main.js
+++ scripts/main.js
@@ -23,9 +23,29 @@ Promise.all([
   loadLevel('1-1')
 ]).then(([sprites, map, level]) => {
   sprites.defineAllSprites(map);
+
+  const backgroundBuffer = document.createElement('canvas');
+  backgroundBuffer.width = GAME_WIDTH * TILE_SIZE;
+  backgroundBuffer.height = GAME_HEIGHT * TILE_SIZE;
+
   level.background.forEach((row, y) => {
     row.forEach((tile, x) => {
-      drawBackground(tile, x, y, ctx, sprites);
+      drawBackground(tile, x, y, backgroundBuffer.getContext('2d'), sprites);
     });
   });
+
+  const playerPos = {
+    x: 2,
+    y: 2,
+  };
+
+  function update() {
+    ctx.drawImage(backgroundBuffer, 0, 0);
+    sprites.drawTile('star', ctx, playerPos.x, playerPos.y);
+    playerPos.x += 0.2;
+    playerPos.y += 0.2;
+    requestAnimationFrame(update);
+  }
+  update();
+
 });
